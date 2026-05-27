252a8981c50b1a28ce7bb0473640540f6e79cbf5
kkk9654-oss <kkk9654@gmail.com>
2026-05-27 15:05:00 +0900

    몬스터 충돌 코드 수정

--- Assets/Scripts/DN_2D/MonsterDamage.cs
+++ Assets/Scripts/DN_2D/MonsterDamage.cs
@@ -4,12 +4,16 @@ public class MonsterDamage : MonoBehaviour
 {
     public int damage = 1;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // 물리 충돌이 발생했을 때 호출됨 (Trigger 아님!)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
+        // 플레이어와 충돌했는지 확인
+        if (collision.collider.CompareTag("Player"))
         {
-            PlayerHP playerHP = collision.GetComponent<PlayerHP>();
+            // 플레이어 HP 스크립트 가져오기
+            PlayerHP playerHP = collision.collider.GetComponent<PlayerHP>();
 
+            // HP 스크립트가 존재하면 데미지 적용
             if (playerHP != null)
             {
                 playerHP.TakeDamage(damage);
